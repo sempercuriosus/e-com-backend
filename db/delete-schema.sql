b33eb79bef44990cae7d7bbe054cdd77edf3d43c
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-20 23:59:27 -0400

    created a delete script

---
+++ db/delete-schema.sql
@@ -0,0 +1,19 @@
+USE ecommerce_db;
+
+-- Drop tables
+DROP TABLE IF EXISTS product_tag
+;
+
+DROP TABLE IF EXISTS category
+;
+
+
+DROP TABLE IF EXISTS product
+;
+
+
+DROP TABLE IF EXISTS tag
+;
+
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
