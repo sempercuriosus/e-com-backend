a00f297e4b614a56b54fae4aace0865742fd16ca
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-21 00:10:29 -0400

    finished model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,25 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+
+    // this is a lookup table relating the two tables product and tag
+    id: {
+      type: DataTypes.INTEGER
+      , allowNull: false
+      , autoIncrement: true
+      , primaryKey: true
+    }
+    , product_id: {
+      type: DataTypes.INTEGER,
+    }
+    , tag_id: {
+      type: DataTypes.INTEGER,
+    }
   },
   {
     sequelize,
