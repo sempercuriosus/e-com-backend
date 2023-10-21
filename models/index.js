60983723a37929f338043ab3af6e676e7ee90286
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-21 13:04:55 -0400

    added the fk and init of the index for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
 
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
